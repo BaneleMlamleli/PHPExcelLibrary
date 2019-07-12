7a45bcdcbdf9f6a9156e50065c276597ec512d5c
BaneleMlamleli <mlamlelibanele@yahoo.com>
2019-07-13 00:51:16 +0200

    Local deletion of MO_Docs

--- dbconnection.php
+++ dbconnection.php
@@ -1,7 +1,8 @@
 <?php
 $con = mysqli_connect("localhost", "root", "", "user");
-var_dump($con);
 if(!$con){
-    echo mysqli_error();
+    echo "Error: Unable to connect to MySQL." . PHP_EOL;
     echo "<script>alert('No database connection established') </script>";
 }
+
+echo "Host information: " . mysqli_get_host_info($link) . PHP_EOL;
