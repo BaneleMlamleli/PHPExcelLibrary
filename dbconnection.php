376b4c620754c12f9d8574033562573d9a047872
BaneleMlamleli <mlamlelibanele@yahoo.com>
2019-07-13 02:02:40 +0200

    error with databaseconnection for some reason

--- dbconnection.php
+++ dbconnection.php
@@ -1,8 +1,11 @@
 <?php
-$con = mysqli_connect("localhost", "root", "", "user");
-if(!$con){
+// Create connection
+$conn = new mysqli("localhost", "root", "", "usermodule");
+
+// Check connection
+if ($conn->connect_error) {
     echo "Error: Unable to connect to MySQL." . PHP_EOL;
     echo "<script>alert('No database connection established') </script>";
 }
 
-echo "Host information: " . mysqli_get_host_info($link) . PHP_EOL;
+echo "Host information: " . mysqli_get_host_info($con) . PHP_EOL;
