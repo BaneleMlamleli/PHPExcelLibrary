e935bd774c50dda35dd2abe130035ffd80db2a09
BaneleMlamleli <G!tL@bB!n@ryM@n01>
2019-07-13 22:41:55 +0200

    database connected. Able to read from Excel sheet

--- dbconnection.php
+++ dbconnection.php
@@ -1,11 +1,8 @@
 <?php
 // Create connection
-$conn = new mysqli("localhost", "root", "", "usermodule");
-
+$con = new mysqli("localhost", "root", "", "usermodule");
 // Check connection
-if ($conn->connect_error) {
+if ($con->connect_error) {
     echo "Error: Unable to connect to MySQL." . PHP_EOL;
     echo "<script>alert('No database connection established') </script>";
 }
-
-echo "Host information: " . mysqli_get_host_info($con) . PHP_EOL;
--- index.php
+++ index.php
@@ -1,3 +1,38 @@
 <?php
 include_once('dbconnection.php');
-echo "Test";
+require_once("PHPExcel/Classes/PHPExcel.php");
+set_time_limit(0);
+ini_set('memory_limit', '-1');
+
+// creating an object of the PHPExcel class
+$excel = new PHPExcel();
+$excel->setActiveSheetIndex(0);
+$excelRow = 1;
+$query = mysqli_query($con, "SELECT * FROM user");
+while($data = mysqli_fetch_object($query)){
+    $excel->getActiveSheet()
+        ->setCellValue('A'.$excelRow, $data->intUserID)
+        ->setCellValue('A'.$excelRow, $data->txtFirstname)
+        ->setCellValue('A'.$excelRow, $data->txtLastname)
+        ->setCellValue('A'.$excelRow, $data->txtIDNumber)
+        ->setCellValue('A'.$excelRow, $data->txtDOB)
+        ->setCellValue('A'.$excelRow, $data->txtGender)
+        ->setCellValue('A'.$excelRow, $data->txtUserType)
+        ->setCellValue('A'.$excelRow, $data->txtEmail)
+        ->setCellValue('A'.$excelRow, $data->txtPassword)
+        ->setCellValue('A'.$excelRow, $data->txtUserDateCreated)
+        ->setCellValue('A'.$excelRow, $data->txtStatus)
+        ->setCellValue('A'.$excelRow, $data->txtUserNote);
+    echo $data->intUserID. ", " .$data->txtFirstname. ", " .$data->txtLastname. ", " .$data->txtIDNumber. ", " .$data->txtDOB
+        .", ".$data->txtGender.", ".$data->txtGender.", ".$data->txtUserType.", ".$data->txtEmail.", ".$data->txtEmail.", ".$data->txtPassword
+        .", ".$data->txtUserDateCreated.", ".$data->txtStatus.", ".$data->txtUserNote."<br>";
+    $excelRow++;
+}
+/* redirect to browser(download) instead of saving to result as a file.
+This is for MS Office Excel xls format*/
+//header('Content-Type: application/cnd.openxmlformats-officedocument.spreadsheet.sheet');
+//header('Content-Disposition: attachment: filename="users.xls"');
+//header('Cache-Control: max-age=0');
+//write the results to a file
+$file = PHPExcel_IOFactory::createWriter($excel, 'Excel2007');
+$file->save('PHPExcelLibrary/users.xls');
