9b8f79e1550198f05543584d6aeeef5575e5b673
BaneleMlamleli <G!tL@bB!n@ryM@n01>
2019-07-14 21:06:30 +0200

    Added composer for dependency management.

--- index.php
+++ index.php
@@ -1,34 +1,21 @@
 <?php
-echo "index.php";
-//include_once('dbconnection.php');
-require_once("PHPExcel/Classes/PHPExcel.php");
+include_once('dbconnection.php');
+require_once("vendor/autoload.php");
+use PhpOffice\PhpSpreadsheet\Spreadsheet;
+use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
 set_time_limit(0);
 ini_set('memory_limit', '-1');
-// creating an object of the PHPExcel class
-$excel = new PHPExcel();
-//$excel->setActiveSheetIndex(0);
-//$excelRow = 1;
-//$query = mysqli_query($con, "SELECT * FROM user");
-//while($data = mysqli_fetch_object($query)){
-//    $excel->getActiveSheet()
-//        ->setCellValue('A'.$excelRow, $data->intUserID)
-//        ->setCellValue('A'.$excelRow, $data->txtFirstname)
-//        ->setCellValue('A'.$excelRow, $data->txtLastname)
-//        ->setCellValue('A'.$excelRow, $data->txtIDNumber)
-//        ->setCellValue('A'.$excelRow, $data->txtDOB)
-//        ->setCellValue('A'.$excelRow, $data->txtGender)
-//        ->setCellValue('A'.$excelRow, $data->txtUserType)
-//        ->setCellValue('A'.$excelRow, $data->txtEmail)
-//        ->setCellValue('A'.$excelRow, $data->txtPassword)
-//        ->setCellValue('A'.$excelRow, $data->txtUserDateCreated)
-//        ->setCellValue('A'.$excelRow, $data->txtStatus)
-//        ->setCellValue('A'.$excelRow, $data->txtUserNote);
-//    echo $data->intUserID. ", " .$data->txtFirstname. ", " .$data->txtLastname. ", " .$data->txtIDNumber. ", " .$data->txtDOB
-//        .", ".$data->txtGender.", ".$data->txtGender.", ".$data->txtUserType.", ".$data->txtEmail.", ".$data->txtEmail.", ".$data->txtPassword
-//        .", ".$data->txtUserDateCreated.", ".$data->txtStatus.", ".$data->txtUserNote."<br>";
-//    $excelRow++;
-//}
-
-$objPHPExcel = PHPExcel_IOFactory::load("/home/shaun/Documents/programming/php/Projects/PHPExcelLibrary/test_file/users.xlsx");
-var_dump($objPHPExcel);
-echo "end of file";
+$excelRow = 1;
+$spreadsheet = new Spreadsheet();
+$sheet = $spreadsheet->getActiveSheet();
+$query = mysqli_query($con, "SELECT * FROM `user` ORDER BY `intUserID` ASC");
+while($data = mysqli_fetch_object($query)){
+    echo $data->intUserID.", ".$data->txtFirstname.", ".$data->txtLastname.", ".$data->txtIDNumber.", ".$data->txtDOB
+        .", ".$data->txtGender.", ".$data->txtUserType.", ".$data->txtEmail.", ".$data->txtPassword.", ".$data->txtUserDateCreated
+        .", ".$data->txtStatus.", ".$data->txtUserNote."<br>";
+    $sheet->setCellValue('A'.$excelRow, $data->txtGender);
+    $excelRow++;
+}
+echo "<hr>";
+$writer = new Xlsx($spreadsheet);
+$writer->save('hello world.xlsx');
